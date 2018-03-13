51998bfc84a38a756c46563f9c6928ba19005f06
Hukiz <36443072+Hukiz@users.noreply.github.com>
2018-03-13 12:46:37 +0000

    Add files via upload

---
+++ Shadow.java
@@ -0,0 +1,32 @@
+/**
+ Name: Shadow
+
+ Description:
+
+ Created by: Hubert Zawistowski
+
+ Created on: 13/03/18
+
+*/
+
+public class Shadow{
+	int x = 1;
+
+	public static void main(String[] args){
+		Shadow s = new Shadow();
+		System.out.println("Local x = "+s.localVar());
+		System.out.println("Gloabl x from method is: "+s.globalVar());
+		System.out.println("Global x from attribute is: "+s.x);
+		Index.main(null);
+ }
+	int globalVar(){
+		return this.x;
+ }
+
+ int localVar(){
+	 int x = 2;
+	 return x;
+
+  }
+
+}
